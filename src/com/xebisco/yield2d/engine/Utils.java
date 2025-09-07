7ffce5faf39b0c7d0e17724306e591e2ce7f8622
Vítor Toledo de Oliveira <vitorxebisco@gmail.com>
2025-09-07 20:45:00 -0300

    Fixed classpath file reading

--- src/com/xebisco/yield2d/engine/Utils.java
+++ src/com/xebisco/yield2d/engine/Utils.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public final class Utils {
 
@@ -15,15 +16,20 @@ public final class Utils {
     }
 
     public static float randomFloat(float min, float max) {
-        if(min == max) return min;
+        if (min == max) return min;
         return RANDOM.nextFloat(min, max);
     }
 
     public static String readStringFromClasspath(String res) throws URISyntaxException, IOException {
         if (!res.startsWith(File.separator)) res = File.separator + res;
-        URL resource = Utils.class.getResource(res);
-        if (resource == null) throw new NullPointerException("Could not find '" + res + "' resource.");
-        return Files.readString(Path.of(resource.toURI()));
+        InputStream in = Utils.class.getResourceAsStream(res);
+        try (in) {
+            if (in == null) throw new NullPointerException("Could not find '" + res + "' resource.");
+            try (InputStreamReader isr = new InputStreamReader(in);
+                 BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        }
     }
 
     public static String readStringFromInputStream(InputStream inputStream) throws IOException {
